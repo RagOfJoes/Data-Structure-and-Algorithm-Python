9a3bdbb25802668b7e8d9fdbeedbc44363507366
RagOfJoes <vhsvragojos@gmail.com>
2019-05-28 01:15:09 -0700

    added comments onto implementation of arrays

--- DataStructures-Arrays/arrayImp.py
+++ DataStructures-Arrays/arrayImp.py
@@ -1,18 +1,20 @@
 # My own implementation of an array
 # Methods: get, push, pop, delete, shift
 class MyArray:
-    def __init__(self):
+    def __init__(self): # Constructor
+        # Properties
         self.length = 0
         self.data = {}
 
-    def get(self, index):
+    # Methods
+    def get(self, index): # Time Complexity O(1)
         return self.data[index]
     
-    def push(self, element):
+    def push(self, element): # Time Complexity O(1)
         self.data[self.length] = element
         self.length += 1
 
-    def pop(self):
+    def pop(self): # Time Complexity O(1)
         try:
             lastItem = self.get(self.length - 1)
         except:
@@ -21,20 +23,23 @@ class MyArray:
         self.length -= 1
         return lastItem
     
-    def delete(self, index):
+    def delete(self, index): # Time Complexity O(n)
         self.shift(index)
         return self.data
 
-    def shift(self, index):
-        for i, value in enumerate(self.data):
+    def shift(self, index): # Time Complexity O(n)
+        for i, value in enumerate(self.data): # enumerate returns iterable object for 
+            # retrieving inddexes and values
             if i >= index:
                 try:
                     self.data[i] = self.data[i + 1]
                 except:
-                    continue
+                    break
         del self.data[self.length - 1]
         self.length -= 1
         return self.data
+
+# Test
 arr = MyArray()
 arr.push(1)
 arr.push(2)
